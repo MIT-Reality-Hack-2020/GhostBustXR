c4989bd95b93258d447e4d322a81b60d88b1ad34
cpapenfuss <chris.papenfuss@data-experts.de>
2020-01-18 11:29:06 -0500

    some already working scripts from before

---
+++ GhostBustXR-AR/Assets/Script/ArTouchHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ArTouchHandler : MonoBehaviour
+{
+
+    public Transform ObjToPlace;
+    private Camera _camera;
+    public ARRaycastManager RayManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (var touch in Input.touches)
+        {
+            if (touch.phase == TouchPhase.Began)
+            {
+                var hitResults = new List<ARRaycastHit>();
+                if (RayManager.Raycast(touch.position, hitResults))
+                {
+                    ObjToPlace.position = hitResults[0].pose.position;
+                    ObjToPlace.gameObject.SetActive(true);
+                }
+                return;
+            }
+        }
+    }
+}
---
+++ GhostBustXR-AR/Assets/Script/RelativeTransformNetworkScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class RelativeTransformNetworkScript : MonoBehaviour, IPunObservable
+{
+
+    public float PositionLerpSpeed = 0.7F;
+    public float RotationLerpSpeed = 0.7F;
+
+    public string RootObjName;
+
+    private GameObject _relativeGameObject;
+
+    private Transform _myTransform;
+
+    public void Awake() => _myTransform = transform;
+
+    Vector3 RelativePosition
+    {
+        get => RelativeGameObject.transform.InverseTransformPoint(_myTransform.position);
+        set => _myTransform.position = Vector3.Lerp(_myTransform.position, RelativeGameObject.transform.TransformPoint(value), PositionLerpSpeed);
+    }
+
+    Quaternion RelativeRotation
+    {
+        get => (Quaternion.Inverse(RelativeGameObject.transform.rotation) * _myTransform.rotation);
+        set => _myTransform.rotation = Quaternion.Lerp(_myTransform.rotation, RelativeGameObject.transform.rotation * value, RotationLerpSpeed);
+    }
+
+    GameObject RelativeGameObject
+    {
+        get
+        {
+            if (_relativeGameObject == null)
+            {
+                _relativeGameObject = GameObject.Find(RootObjName);
+            }
+            return (_relativeGameObject);
+        }
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(RelativePosition);
+            stream.SendNext(RelativeRotation);
+        }
+        else
+        {
+            RelativePosition = (Vector3)stream.ReceiveNext();
+            RelativeRotation = (Quaternion)stream.ReceiveNext();
+        }
+    }
+}
