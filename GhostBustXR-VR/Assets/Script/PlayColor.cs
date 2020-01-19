64487e825d78a80acfc85637264d8fd323020c53
cpapenfuss <chris.papenfuss@data-experts.de>
2020-01-18 20:37:15 -0500

    enabling networking for VR only AR->VR

---
+++ GhostBustXR-VR/Assets/Script/PlayColor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayColor : MonoBehaviour
+{
+    public GameObject[] Colors;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Colors[new System.Random().Next(0, Colors.Length)].SetActive(true);
+    }
+
+}
