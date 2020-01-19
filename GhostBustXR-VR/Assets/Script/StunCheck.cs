4341f275b0775fcb4a705b2709ae9ff18416a833
cpapenfuss <chris.papenfuss@data-experts.de>
2020-01-19 01:09:53 -0500

    stun implemented

---
+++ GhostBustXR-VR/Assets/Script/StunCheck.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class StunCheck : MonoBehaviour, IPunObservable
+{
+    private bool _isStunned = true;
+    public float StunCooldown = 2F;
+    public float PushbackDistance = 1f;
+    public UnityEvent Stunned;
+    public UnityEvent Released;
+
+    private void OnTriggerStay(Collider other)
+    {
+        _isStunned = true;
+        Stunned.Invoke();
+        transform.localPosition = transform.localPosition + Vector3.Scale(other.transform.forward, new Vector3(1f, 0f, 1f)).normalized * PushbackDistance;
+        Invoke("Release", StunCooldown);
+    }
+
+    public void Release()
+    {
+        _isStunned = false;
+        Released.Invoke();
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(_isStunned);
+        }
+        else
+        {
+            _isStunned = (bool)stream.ReceiveNext();
+        }
+    }
+}
